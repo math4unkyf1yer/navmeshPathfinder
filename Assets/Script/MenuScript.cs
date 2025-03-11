fe1f81a7f18b6e86c5e7e116067816d4c019afb3
math4unkyf1yer <mathcastonguay2004@gmail.com>
2025-03-11 04:00:09 -0400

    credit and volume page

--- Assets/Script/MenuScript.cs
+++ Assets/Script/MenuScript.cs
@@ -7,6 +7,7 @@ public class MenuScript : MonoBehaviour
 {
     public GameObject volumePage;
     public GameObject creditPage;
+    public GameObject[] closeItem;
     public void OnClickExit()
     {
         Application.Quit();//exit the game
@@ -18,9 +19,26 @@ public class MenuScript : MonoBehaviour
     public void OnClickVolume()
     {
         volumePage.SetActive(true);
+        foreach (GameObject item in closeItem)
+        {
+            item.SetActive(false);
+        }
     }
     public void OnClickCredit()
     {
         creditPage.SetActive(true);
+        foreach (GameObject item in closeItem)
+        {
+            item.SetActive(false);
+        }
+    }
+    public void OnClickBackToMenu()
+    {
+        foreach (GameObject item in closeItem)
+        {
+            item.SetActive(true);
+        }
+        creditPage.SetActive(false);
+        volumePage.SetActive(false);
     }
 }
